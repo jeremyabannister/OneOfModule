8b4d0606fab9ffeb662c243257c4647e24d9fca0
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-10-24 11:26:41 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# OneOfModule
+
+A description of this package.
---
+++ Tests/OneOfModuleTests/OneOfModuleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import OneOfModule
+
+final class OneOfModuleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(OneOfModule().text, "Hello, World!")
+    }
+}
