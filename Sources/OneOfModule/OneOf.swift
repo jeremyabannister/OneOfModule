e6383250713850448efce6b63ff58a56b7ae2752
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-10-24 11:30:00 +0200

    Added `OneOf1` through `OneOf10`.

---
+++ Sources/OneOfModule/OneOf.swift
@@ -0,0 +1,101 @@
+//
+//  OneOf.swift
+//  
+//
+//  Created by Jeremy Bannister on 10/24/22.
+//
+
+///
+public enum OneOf1 <T0> {
+    case first (T0)
+}
+
+///
+public enum OneOf2 <T0, T1> {
+    case first (T0)
+    case second (T1)
+}
+
+///
+public enum OneOf3 <T0, T1, T2> {
+    case first (T0)
+    case second (T1)
+    case third (T2)
+}
+
+///
+public enum OneOf4 <T0, T1, T2, T3> {
+    case first (T0)
+    case second (T1)
+    case third (T2)
+    case fourth (T3)
+}
+
+///
+public enum OneOf5 <T0, T1, T2, T3, T4> {
+    case first (T0)
+    case second (T1)
+    case third (T2)
+    case fourth (T3)
+    case fifth (T4)
+}
+
+///
+public enum OneOf6 <T0, T1, T2, T3, T4, T5> {
+    case first (T0)
+    case second (T1)
+    case third (T2)
+    case fourth (T3)
+    case fifth (T4)
+    case sixth (T5)
+}
+
+///
+public enum OneOf7 <T0, T1, T2, T3, T4, T5, T6> {
+    case first (T0)
+    case second (T1)
+    case third (T2)
+    case fourth (T3)
+    case fifth (T4)
+    case sixth (T5)
+    case seventh (T6)
+}
+
+///
+public enum OneOf8 <T0, T1, T2, T3, T4, T5, T6, T7> {
+    case first (T0)
+    case second (T1)
+    case third (T2)
+    case fourth (T3)
+    case fifth (T4)
+    case sixth (T5)
+    case seventh (T6)
+    case eigth (T7)
+}
+
+///
+public enum OneOf9 <T0, T1, T2, T3, T4, T5, T6, T7, T8> {
+    case first (T0)
+    case second (T1)
+    case third (T2)
+    case fourth (T3)
+    case fifth (T4)
+    case sixth (T5)
+    case seventh (T6)
+    case eigth (T7)
+    case ninth (T8)
+}
+
+///
+public enum OneOf10 <T0, T1, T2, T3, T4, T5, T6, T7, T8, T9> {
+    case first (T0)
+    case second (T1)
+    case third (T2)
+    case fourth (T3)
+    case fifth (T4)
+    case sixth (T5)
+    case seventh (T6)
+    case eigth (T7)
+    case ninth (T8)
+    case tenth (T9)
+}
--- Sources/OneOfModule/OneOfModule.swift
+++
@@ -1,6 +0,0 @@
-public struct OneOfModule {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
